9aea1afada73327e1399b18e1035608bea096617
Rodrigo Burgos <rodrigoburgos@live.com>
2023-03-02 23:20:24 -0300

    general service

---
+++ general.service.js
@@ -0,0 +1,39 @@
+const { queryFee, queryLiquidityTransactions, querySwapTransactions, queryTVL } = require('./functions')
+
+const getFees = async () => {
+    try {
+        const result = await queryFee()
+        return result
+    } catch (error) {
+        console.log(error, 'for getFees')
+    }
+}
+
+const getLiquidityTx = async (limit) => {
+    try {
+        const result = await queryLiquidityTransactions(limit)
+        return result
+    } catch (error) {
+        console.log(error, 'for getLiquidityTx')
+    }
+}
+
+const getSwapTx = async (limit) => {
+    try {
+        const result = await querySwapTransactions(limit)
+        return result
+    } catch (error) {
+        console.log(error, 'for getLiquidityTx')
+    }
+}
+
+const getTvl = async (limit) => {
+    try {
+        const result = await queryTVL(limit)
+        return result
+    } catch (error) {
+        console.log(error, 'for getLiquidityTx')
+    }
+}
+
+module.exports = { getFees, getLiquidityTx, getFees, getTvl, getSwapTx }
