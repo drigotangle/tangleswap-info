7ab9ae3f277815bcd55994d59bdc271bb284f7a3
Rodrigo Burgos <rodrigoburgos@live.com>
2023-03-16 01:39:34 -0300

    state

--- src/state/Actions.ts
+++ src/state/Actions.ts
@@ -1,26 +1,26 @@
 import { GroupedEntry, IPoolData, IPoolLiquidity, IToken, ITVL, ITx } from "../interfaces"
 
 
-export const setTVL = (dispatch: any, payload: ITVL[] | GroupedEntry[] | IPoolLiquidity[]) => {
+export const setTVL = (dispatch: any, payload: ITVL[] | GroupedEntry[] | IPoolLiquidity[]): void => {
     dispatch({type: 'SET_LIQUIDITY_DATA', payload: payload})
 }
 
-export const setLiquidtyBarData = (dispatch: any, payload: ITVL[] | GroupedEntry[] | IPoolLiquidity[]) => {
+export const setLiquidtyBarData = (dispatch: any, payload: ITVL[] | GroupedEntry[] | IPoolLiquidity[]): void => {
     dispatch({type: 'SET_LIQUIDITY_BAR_DATA', payload: payload})
 }
 
-export const setTokenData = (dispatch: any, payLoad: IToken[]) => {
+export const setTokenData = (dispatch: any, payLoad: IToken[]): void => {
     dispatch({type: 'SET_TOKEN_DATA', payload: payLoad})
 }
 
-export const setPoolData = (dispatch: any, payLoad: IPoolData[]) => {
+export const setPoolData = (dispatch: any, payLoad: IPoolData[]): void => {
     dispatch({type: 'SET_POOL_DATA', payload: payLoad})
 }
 
-export const setTxData = (dispatch: any, payLoad: ITx[]) => {
+export const setTxData = (dispatch: any, payLoad: ITx[]): void => {
     dispatch({type: 'SET_TX_DATA', payload: payLoad})
 }
 
-export const setChain = (dispatch: any, payLoad: string | undefined) => {
+export const setChain = (dispatch: any, payLoad: string | undefined): void => {
     dispatch({type: 'SET_CHAIN', payload: payLoad})
 }
