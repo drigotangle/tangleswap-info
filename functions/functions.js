195eab6aed7e515fee009e5f15b29aa738083679
Rodrigo Burgos <drigo@tangleswap.exchange>
2023-04-01 19:32:37 -0300

    poolArr fix

--- functions/functions.js
+++ functions/functions.js
@@ -137,9 +137,9 @@ const sqrtPriceToPrice = (price) => {
 const queryPools = async (limit) => {
     try {
         const collection = await mongoClient.db("tangle-db-shimmer").collection("pools")
+        const poolsArr = []
+        const poolSet = new Set()
         if(limit !== undefined){
-            const poolsArr = []
-            const poolSet = new Set()
             const documents = await collection.find({})
             .sort({ blockNumber: 1 }) // Sort by blockNumber in descending order
             .limit(limit) // Limit to the first 10 results
