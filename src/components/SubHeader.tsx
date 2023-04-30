b3fa4a17b5f39b71128446c3d902a63a8d7940ba
Rodrigo Burgos <drigo@tangleswap.exchange>
2023-04-30 16:36:32 -0300

    Update SubHeader.tsx

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState, FC } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 import { Stack, Chip, Typography } from '@mui/material'
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import styled from 'styled-components'
@@ -26,95 +26,55 @@ const SubHeader = () => {
     const { state, dispatch } = useContext(AppContext)
     const { chain, usdPrice } = state
     const theme = useTheme();
-    
 
-    const loadDataFromLocalStorage = useCallback(
-        (setLoading: (loading: boolean) => void) => {
-            const storedData = localStorage.getItem('data');
-            if (storedData) {
-                const {
-                    tvl,
-                    usdPrice,
-                    pools,
-                    tokens,
-                    liquidityTx,
-                    swapTx,
-                } = JSON.parse(storedData);
+    const fetchData = async () => {
+        try {
+            const [
+                tvl,
+                usdPrice,
+                pools,
+                tokens,
+                liquidityTx,
+                swapTx,
+            ] = await Promise.all([
+                getTVL(1000, chain),
+                getUsdPrice(chain),
+                getPools(1000, chain),
+                getTokens(chain),
+                getLiquidityTx(20, chain),
+                getSwapTx(20, chain),
+            ]);
 
-                setTVL(dispatch, tvl);
-                setUsdPrice(dispatch, Number(usdPrice.USD));
-                setPoolData(dispatch, pools);
-                setTokenData(dispatch, tokens);
-                setTxData(dispatch, [...liquidityTx, ...swapTx]);
-            }
-        },
-        [dispatch],
-    );
+            const firstPriceArr = pools[0].price;
+            const firstLiquidityArr = pools[0].liquidity;
+            const lastBlockFromLiquidityTx = liquidityTx[liquidityTx.length - 1].blockNumber;
+
+            setLastBlockSync(lastBlockFromLiquidityTx);
+            setUsdPrice(dispatch, Number(usdPrice.USD));
+
+            // Update the state with the fetched data
+            setTVL(dispatch, tvl);
+            setPoolData(dispatch, pools);
+            setTokenData(dispatch, tokens);
+            setTxData(dispatch, [...liquidityTx, ...swapTx]);
+
+            setLoading(false); // Set loading to false when the data is loaded
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
     useEffect(() => {
-        loadDataFromLocalStorage(setLoading);
-
-        const fetchData = async () => {
-            try {
-                const [
-                    tvl,
-                    usdPrice,
-                    pools,
-                    tokens,
-                    liquidityTx,
-                    swapTx,
-                ] = await Promise.all([
-                    getTVL(1000, chain),
-                    getUsdPrice(chain),
-                    getPools(1000, chain),
-                    getTokens(chain),
-                    getLiquidityTx(20, chain),
-                    getSwapTx(20, chain),
-                ]);
-
-                const firstPriceArr = pools[0].price;
-                const firstLiquidityArr = pools[0].liquidity;
-                const lastBlockFromLiquidityTx = liquidityTx[liquidityTx.length - 1].blockNumber;
-
-                setLastBlockSync(lastBlockFromLiquidityTx);
-                setUsdPrice(dispatch, Number(usdPrice.USD));
-
-                // Store the fetched data in local storage
-                localStorage.setItem('data', JSON.stringify({
-                    tvl,
-                    usdPrice,
-                    pools,
-                    tokens,
-                    liquidityTx,
-                    swapTx,
-                }));
-
-                // Update the state with the fetched data
-                setTVL(dispatch, tvl);
-                setPoolData(dispatch, pools);
-                setTokenData(dispatch, tokens);
-                setTxData(dispatch, [...liquidityTx, ...swapTx]);
-
-                setLoading(false); // Set loading to false when the data is loaded
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
         fetchData();
-    }, [chain, dispatch, loadDataFromLocalStorage, setLoading]);
+    }, [chain, dispatch]);
 
-    useEffect(() => {
-        // Schedule local storage deletion every 5 minutes
-        const deleteLocalStorageInterval = setTimeout(() => {
-            localStorage.removeItem('data');
-        }, 3 * 60 * 1000);
-
-        // Cleanup function to clear the timeout when the component is unmounted
-        return () => {
-            clearTimeout(deleteLocalStorageInterval);
-        };
-    }, []);
+    if(!loading){
+        return (
+            <HeadWrapper>
+                <Typography variant='subtitle1'>Loading...</Typography>
+            </HeadWrapper>
+        )        
+    }
 
     return (
         <HeadWrapper>
@@ -128,4 +88,4 @@ const SubHeader = () => {
     )
 }
 
-export default SubHeader
+export default SubHeader;
